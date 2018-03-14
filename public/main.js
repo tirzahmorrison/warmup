3fea91f23396b29239a8a9d22c7cc8c020735c12
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-03-14 08:28:03 -0400

    color

--- public/main.js
+++ public/main.js
@@ -1,5 +1,8 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
-
-document.addEventListener('DOMContentLoaded', main)
+(function () {
+  const result = '#', i = 0;
+  while (i < 6) {
+  result += (Math.floor(Math.random()*16)).toString(16);
+  i++;
+  }
+  document.body.style.background = result;
+  })();
